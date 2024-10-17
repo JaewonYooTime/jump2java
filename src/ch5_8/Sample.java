bbf218683d89ffe66672b3e44ee3e53e6b649907
JaewonYooTime <dkrkvudghk@naver.com>
2024-10-17 23:51:06 +0900

    24.10.17

---
+++ src/ch5_8/Sample.java
@@ -0,0 +1,65 @@
+package ch5_8;
+
+interface Predator{
+    String getFood();
+
+    default void printFood(){
+        System.out.printf("my food is %s\n", getFood());
+    }
+
+    int LEG_COUNT = 4; // 인터페이스 상수
+
+    static int speed(){
+        return LEG_COUNT * 30;
+    }
+}
+
+class Animal{
+    String name;
+
+    void setName(String name){
+        this.name = name;
+    }
+}
+
+class Tiger extends Animal implements Predator{
+    public String getFood(){
+        return "apple";
+    }
+}
+
+class Lion extends Animal implements Predator{
+    public String getFood(){
+        return "banana";
+    }
+}
+
+class ZooKeeper{
+    void feed(Predator predator){
+        System.out.println("feed " + predator.getFood());
+    }
+}
+
+class Bouncer {
+    void barkAnimal(Animal animal){
+        if (animal instanceof Tiger){
+            System.out.println("어흥");
+        } else if (animal instanceof Lion){
+            System.out.println("으르렁");
+        }
+    }
+}
+
+
+public class Sample {
+    public static void main(String[] args){
+        ZooKeeper zooKeeper = new ZooKeeper();
+        Tiger tiger = new Tiger();
+        Lion lion  = new Lion();
+
+        Bouncer bouncer = new Bouncer();
+        bouncer.barkAnimal(tiger);
+        bouncer.barkAnimal(lion);
+
+    }
+}
